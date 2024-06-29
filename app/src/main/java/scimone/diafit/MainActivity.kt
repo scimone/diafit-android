6edae608a858341149c3f4cd1b63901ca0acd22a
scimone <anna.scimone@live.de>
2024-06-29 17:14:10 +0200

    Initial commit

--- app/src/main/java/scimone/diafit/MainActivity.kt
+++ app/src/main/java/scimone/diafit/MainActivity.kt
@@ -1,5 +1,6 @@
 package scimone.diafit
 
+import android.content.Context
 import android.content.IntentFilter
 import android.os.Bundle
 import android.util.Log
@@ -17,27 +18,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import scimone.diafit.ui.theme.DiafitTheme
 
-// MainActivity extends ComponentActivity and implements OnUpdateListener
 class MainActivity : ComponentActivity(), CGMReceiver.OnUpdateListener {
-
-    // Mutable state for holding the CGM value
     private val cgmValue = mutableStateOf("")
 
-    // Instance of CGMReceiver
     private var cgmReceiver: CGMReceiver? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Initialize CGMReceiver and set MainActivity as listener
         cgmReceiver = CGMReceiver()
         cgmReceiver?.setOnUpdateListener(this)
-
-        // Register CGMReceiver to listen for broadcasts with intent filter "glucodata.Minute"
-        registerReceiver(cgmReceiver, IntentFilter("glucodata.Minute"))
+        registerReceiver(cgmReceiver, IntentFilter("glucodata.Minute"), RECEIVER_EXPORTED)
         Log.i("MainActivity", "Receiver registered")
 
-        // Set the content of the activity using Jetpack Compose
         setContent {
             DiafitTheme {
                 // A surface container using the 'background' color from the theme
@@ -46,10 +38,7 @@ class MainActivity : ComponentActivity(), CGMReceiver.OnUpdateListener {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Column {
-                        // Display a greeting
                         Greeting("Anna")
-
-                        // Display the latest CGM value
                         NewestCGMValue(cgmValue.value)
                     }
                 }
@@ -57,24 +46,18 @@ class MainActivity : ComponentActivity(), CGMReceiver.OnUpdateListener {
         }
     }
 
-    // Callback method called by CGMReceiver when a new glucose value is received
     override fun onUpdate(newCgmValue: Float) {
         Log.d("MainActivity", "New glucose value received: $newCgmValue")
-
-        // Update the UI with the new CGM value
         cgmValue.value = newCgmValue.toString()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-
-        // Unregister the receiver when the activity is destroyed to prevent leaks
         unregisterReceiver(cgmReceiver)
         Log.i("MainActivity", "Receiver unregistered")
     }
 }
 
-// Composable function to display a greeting
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -83,7 +66,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-// Composable function to display the newest CGM value
 @Composable
 fun NewestCGMValue(cgmValue: String, modifier: Modifier = Modifier) {
     Text(
@@ -92,7 +74,6 @@ fun NewestCGMValue(cgmValue: String, modifier: Modifier = Modifier) {
     )
 }
 
-// Preview function for Greeting composable
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
